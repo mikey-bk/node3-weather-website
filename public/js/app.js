203d1ea9956250f3669b32d9b00556b3f1f1c0bc
Michael Guenot <michael.guenot@gmail.com>
2019-03-26 12:09:14 -0400

    Remove unnecessary space

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ const search = document.querySelector('input');
 const message1 = document.querySelector('#message-1');
 const message2 = document.querySelector('#message-2');
 
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
