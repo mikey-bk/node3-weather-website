64c53388373f242005220bb23e8c909f7c15cb67
Michael Guenot <michael.guenot@gmail.com>
2019-03-26 12:46:32 -0400

    Prepared files for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '..', 'public');
@@ -111,7 +112,7 @@ app.get('*', (req,res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000.');
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.`);
 });
 
